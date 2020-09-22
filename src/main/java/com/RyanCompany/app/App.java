24f08bcee80c94e43d74c6943617838d7cbc6120
Ryan Meoni <rmeon001@ucr.edu>
2020-09-22 13:38:32 -0700

    Created Calculator class, moved most of implementation there

--- src/main/java/com/RyanCompany/app/App.java
+++ src/main/java/com/RyanCompany/app/App.java
@@ -4,102 +4,33 @@ import java.util.*;
 
 public class App {
 
-    public static void main(String[] args) {
-	while (true)
-	{
-	System.out.println("Enter new calculator operation in format '3 + 3 - 2', with a space inbetween each argument");
-        //Get input line from console, split input line by whitespace (later parentheses as well) delimiters
-        Scanner scan = new Scanner(System.in);
-        String inputLine = scan.nextLine();
-        String[] inputTokens = inputLine.split("\\s+");
+    public static void main(String[] args)
+    {
+        // Calculator interface
+        Calculator theCalc = new Calculator();
 
-        //Two arrayLists to hold operators and operands in order. Used to build tree later
-        List<String> operatorList = new ArrayList<String>();
-        List<Double> operandList = new ArrayList<Double>();
-        //Variables used to help parse input
-        boolean validInput = true;
-        double Number = Double.MIN_VALUE;
-
-        for (int i = 0; i < inputTokens.length; i++)
+        while (true)
         {
-            //Handle operators
-            if (inputTokens[i].equals("+") || inputTokens[i].equals("-") || inputTokens[i].equals("*") || inputTokens[i].equals("/"))
-            {
-                operatorList.add(inputTokens[i]);
-            }
-            //Handle operands
-            else
-            {
-                try
-                {
-                    Number = Double.parseDouble(inputTokens[i]);
-                }
+            // Get input string from console
+            System.out.println("Enter new calculator operation in format '3 + 3 - 2', with a space inbetween each argument");
+            Scanner scan = new Scanner(System.in);
+            String inputLine = scan.nextLine();
 
-                catch (NumberFormatException e)
-                {
-                    validInput = false;
-                }
+            // Get parsed input line, set up fields for tree building
+            List<List<String>> parsedInput = theCalc.parseInput(inputLine);
+            if (parsedInput != null && parsedInput.size() == 2)
+            {
+                List<String> operandList = parsedInput.get(0);
+                List<String> operatorList = parsedInput.get(1);
 
-                finally
+                // Build expression tree from parsed input line
+                commandArg treeRoot = theCalc.buildTree(operandList, operatorList);
+
+                if (treeRoot != null)
                 {
-                    if (validInput)
-                    {
-                        operandList.add(Number);
-                    }
+                    System.out.println(treeRoot.compute());
                 }
             }
-        }
-
-        //Another simple check for valid input, operand count must be exactly 1 more than the operator count.
-        if (operandList.size() - 1 != operatorList.size())
-        {
-            validInput = false;
-        }
-
-        if (validInput)
-        {
-            commandArg leftChild = null;
-            commandArg RightChild = null;
-            commandArg root = null;
-
-            for (int i = 0; i < operatorList.size(); i++)
-            {
-                //The first operator in operatorList needs the first two operands in operand list.
-                //This is only true for very first iteration. In other iterations, we build the
-                //expression tree moving upwards.
-                if (i == 0)
-                {
-                    Operand currLeftOperand = new Operand(operandList.get(0));
-                    Operand currRightOperand = new Operand(operandList.get(1));
-
-                    String firstOperator = operatorList.get(0);
-
-                    if (firstOperator.equals("+")) root = new Add(currLeftOperand, currRightOperand);
-                    if (firstOperator.equals("-")) root = new Subtract(currLeftOperand, currRightOperand);
-                    if (firstOperator.equals("/")) root = new Divide(currLeftOperand, currRightOperand);
-                    if (firstOperator.equals("*")) root = new Multiply(currLeftOperand, currRightOperand);
-                    leftChild = root;
-                }
-
-
-                //For every following operator, the previous operator node becomes the new left child, and the
-                //next operand becomes the right child.
-                else
-                {
-                    //We use the i + 1 index to get each following operand due to getting two operands
-                    //in the very first iteration of the loop.
-                    Operand currRightOperand = new Operand(operandList.get(i + 1));
-                    String currOperator = operatorList.get(i);
-                    if (currOperator.equals("+")) root = new Add(leftChild, currRightOperand);
-                    if (currOperator.equals("-")) root = new Subtract(leftChild, currRightOperand);
-                    if (currOperator.equals("/")) root = new Divide(leftChild, currRightOperand);
-                    if (currOperator.equals("*")) root = new Multiply(leftChild, currRightOperand);
-                    leftChild = root;
-                }
-            }
-
-            System.out.println(root.compute());
-        }
         }
     }
 }
---
+++ src/main/java/com/RyanCompany/app/Calculator.java
@@ -0,0 +1,120 @@
+package com.RyanCompany.app;
+
+import java.util.*;
+
+public class Calculator
+{
+    Calculator() {}
+
+    // Input: Console input line as a String
+    // Output: List of two lists, one for operands and one for operators
+    public List<List<String>> parseInput(String inputLine)
+    {
+        List<List<String>> result = new ArrayList<List<String>>();
+
+        // Split input line by whitespace delimiters
+        String[] inputTokens = inputLine.split("\\s+");
+
+        // Two arrayLists to hold operators and operands in order. Used to build tree later
+        List<String> operatorList = new ArrayList<String>();
+        List<String> operandList = new ArrayList<String>();
+        // Variables used to help parse input
+        boolean validInput = true;
+        double Number = Double.MIN_VALUE;
+
+        for (int i = 0; i < inputTokens.length; i++)
+        {
+            String currToken = inputTokens[i];
+            // Handle operators
+            if (currToken.equals("+") || currToken.equals("-") || currToken.equals("*") || currToken.equals("/"))
+            {
+                operatorList.add(currToken);
+            }
+
+            // Handle operands, and checks for invalid input
+            else
+            {
+                // Check if current token is a valid number
+                try
+                {
+                    Number = Double.parseDouble(currToken);
+                }
+                catch (NumberFormatException e)
+                {
+                    validInput = false;
+                }
+
+                if (validInput)
+                {
+                    operandList.add(currToken);
+                }
+
+                // Immediately return null if invalid input
+                else
+                {
+                    return null;
+                }
+
+            }
+        }
+        // Return a list containing the operand list and the operator list
+        result.add(operandList);
+        result.add(operatorList);
+        return result;
+    }
+
+    // Input: List of operands in order and list of operators in order
+    // Output: List of two lists, one for operands and one for operators
+    public commandArg buildTree(List<String> operandList, List<String> operatorList)
+    {
+        if (operandList == null || operatorList == null) return null;
+
+        // Check that operand count is exactly 1 more than the operator count, or for a single operand
+        if (operandList.size() - 1 != operatorList.size() || operandList.size() <= 1)
+        {
+            return null;
+        }
+
+        commandArg leftChild = null;
+        commandArg RightChild = null;
+        commandArg root = null;
+        for (int i = 0; i < operatorList.size(); i++)
+        {
+            //The first operator in operatorList needs the first two operands in operand list.
+            //This is only true for very first iteration. In other iterations, we build the
+            //expression tree moving upwards.
+            if (i == 0)
+            {
+                Double leftOperandValue = Double.parseDouble(operandList.get(0));
+                Double rightOperandValue = Double.parseDouble(operandList.get(1));
+                Operand currLeftOperand = new Operand(leftOperandValue);
+                Operand currRightOperand = new Operand(rightOperandValue);
+
+                String firstOperator = operatorList.get(0);
+
+                if (firstOperator.equals("+")) root = new Add(currLeftOperand, currRightOperand);
+                if (firstOperator.equals("-")) root = new Subtract(currLeftOperand, currRightOperand);
+                if (firstOperator.equals("/")) root = new Divide(currLeftOperand, currRightOperand);
+                if (firstOperator.equals("*")) root = new Multiply(currLeftOperand, currRightOperand);
+                leftChild = root;
+            }
+
+
+            //For every following operator, the previous operator node becomes the new left child, and the
+            //next operand becomes the right child.
+            else {
+                //We use the i + 1 index to get each following operand due to getting two operands
+                //in the very first iteration of the loop.
+                Double rightOperandValue = Double.parseDouble(operandList.get(i + 1));
+                Operand currRightOperand = new Operand(rightOperandValue);
+                String currOperator = operatorList.get(i);
+                if (currOperator.equals("+")) root = new Add(leftChild, currRightOperand);
+                if (currOperator.equals("-")) root = new Subtract(leftChild, currRightOperand);
+                if (currOperator.equals("/")) root = new Divide(leftChild, currRightOperand);
+                if (currOperator.equals("*")) root = new Multiply(leftChild, currRightOperand);
+                leftChild = root;
+            }
+        }
+        return root;
+    }
+}
